431ac19e0f475bc37f64d4e7dfcfdfc5af3cf7ae
thyagovis <thyagosilva321123@gmail.com>
2026-06-15 13:35:30 -0300

    feat: validação nos dto de request

--- src/main/java/com/stockflow/StockFlowApi/solicitacaoCompra/dto/item/ItemSolicitacaoCompraResponseDTO.java
+++ src/main/java/com/stockflow/StockFlowApi/solicitacaoCompra/dto/item/ItemSolicitacaoCompraResponseDTO.java
@@ -1,5 +1,8 @@
 package com.stockflow.StockFlowApi.solicitacaoCompra.dto.item;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record ItemSolicitacaoCompraResponseDTO(
--- src/main/java/com/stockflow/StockFlowApi/solicitacaoCompra/dto/solicitacao/SolicitacaoCompraRequestDTO.java
+++ src/main/java/com/stockflow/StockFlowApi/solicitacaoCompra/dto/solicitacao/SolicitacaoCompraRequestDTO.java
@@ -2,12 +2,20 @@ package com.stockflow.StockFlowApi.solicitacaoCompra.dto.solicitacao;
 
 import com.stockflow.StockFlowApi.solicitacaoCompra.dto.item.ItemSolicitacaoCompraRequestDTO;
 import com.stockflow.StockFlowApi.solicitacaoCompra.entity.ItemSolicitacaoCompra;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record SolicitacaoCompraRequestDTO(
+        @NotNull
         Long usuario_id,
+
+        @NotBlank
         String obs,
+
+        @NotEmpty
         List<ItemSolicitacaoCompraRequestDTO> itensCompra
 ) {
 }
